fe9436bee847ae586a0d52e7ff0b9aacffab9cb1
Ochuba Ekene Alexander <147551927+OchubaEkene@users.noreply.github.com>
2024-03-31 16:34:19 +0100

    Update Image & text chatbot displayed in Terminal.py

--- Image & text chatbot displayed in Terminal.py
+++ Image & text chatbot displayed in Terminal.py
@@ -1,1 +1,13 @@
+import os
+import google.generativeai as genai
 
+os.environ['GOOGLE_API_KEY'] = "YOUR API KEY"
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+vision_model = genai.GenerativeModel('gemini-pro-vision')
+
+import PIL.Image
+image = PIL.Image.open('ANY PICTURE OF YOUR CHOICE')
+
+response = vision_model.generate_content(["Explain this image", image])
+print(response.text)
